fab4c373dea2dd8919fae7c89093dd5cb81e6c20
Samkelwa <samkelwa101@gmail.com>
2025-07-01 00:18:10 +0200

    Completed the fifth exercise as well.

---
+++ ex5.py
@@ -0,0 +1,24 @@
+# Exercise 5
+
+name = "Samkelwa G. Gule"
+age = 26 # Not a lie
+height = 1.5 # metres
+weight = 50 # Kilograms
+eyes = "Dark brown"
+teeth = "White"
+hair = "Black"
+
+weight_lbs = weight * 2.2
+height_inchs = height * 39.36
+
+print(f"Let's talk about {name}.")
+print(f"He's {height}m tall.")
+print(f"He's {weight}kg heavy.")
+print("Actually, that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+print(f"My height in inches is {height_inchs}in and my weight in pounds is {weight_lbs}lbs.")
