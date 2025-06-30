6523501d78f9a346eaeac8ea2be490ab5b63823e
Samkelwa <samkelwa101@gmail.com>
2025-06-30 22:38:17 +0200

    Initial commit with the first 3 complete projects and half the fourth.

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# print("Hello, World!")
+# print("Hello again")
+# print("I like typing this")
+# print("This is fun")
+# print('Yay! Printing.')
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this.')
+print("print another line...")
+
+# This is an octothorpe (or "hash", or "pound", or "mesh")
---
+++ ex3.py
@@ -0,0 +1,40 @@
+# NUMBERS
+
+# This line prints the string
+print("I will now count my chickens:")
+
+# These lines print a string and a calculation simultaneously
+print("Hens", 25 + 30 / 6)
+
+# In this calcuation; The first operation is the multiplication (*) which yields an answer (75)
+# then the modulus operator finds the remainder of 75/4 (it's 3) and only then is it subtracted from 100 to get the answer 97
+print("Roosters", 100 - 25 * 3 % 4)
+
+# This line prints a string
+print("Now I will count the eggs:")
+
+# Another long operation, but the BODMAS rules apply so division first, then modulus then the others
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# String printing what operation i'll be doing in the next line
+print("Is it true that 3 + 2 < 5 - 7?")
+# A calculation occurs on either side of the comparison operator (<),
+# then hte answers are compared to see if the left is greater than the right and then it retursn false because iot isn't
+print(3 + 2 < 5 - 7)
+
+# Prints both string and the resut of the calculation
+print("What is 3 +  2? ", 3 + 2)
+print("What is 5 - 7? ", 5 - 7)
+
+# Prints string
+print("Oh, that's why it's false.")
+
+# Prints string
+print("How about some more.")
+
+# Prints both string and the resut of the calculation
+print("Is it greater? ", 5 > -2)
+# Prints both string and the resut of the calculation
+print("Is it greater or equal? ", 5 >= -2)
+# Prints both string and the resut of the calculation
+print("Is it less or equal? ", 5 <= -2)
