01f8ffe416be0f92c47312e4fde97ff04f577e5b
Samkelwa <samkelwa101@gmail.com>
2025-07-02 21:40:25 +0200

    Finished exercise 9 with all its study drills.

--- ex9.py
+++ ex9.py
@@ -5,10 +5,13 @@ days = "Mon Tue Wed Thu Fri Sat Sun"
 # Here we create a variable storing the months, each on a different line with the '\n' character
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
 
-#
+# Here we display the day variable after a short string explaining what is beng displayed
 print("Here are the days: ", days)
+# Here we displayb the months after a short string explaining what is being displayed
 print("Here are the months: ", months)
 
+# Here we display multiple lines at once using the three double-quotes
+# This format is best for displaying paragraphs, what is printed in between is displayed exactly as printed with each space, tab (indent) exactly as it appears in the code
 print("""
 There's something going on here.
 With the three double-quotes.
