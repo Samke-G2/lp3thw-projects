f1726e97502430431f186505c094b30597e496cf
Samkelwa <samkelwa101@gmail.com>
2025-07-02 23:49:26 +0200

    started and coompleted exercise 11, sans the study drills or testing output.

---
+++ ex11.py
@@ -0,0 +1,10 @@
+# Exercise 11 -  Asking Questions
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you? (in m)", end=' ')
+height = input()
+print("How much do you weigh? (in Kg)", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height}m tall and {weight}kg heavy.")
