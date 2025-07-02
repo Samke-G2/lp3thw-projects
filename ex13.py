ec0fcc4253d2d2fcd825688bc7ee7b4c6975b96f
Samkelwa <samkelwa101@gmail.com>
2025-07-03 01:42:38 +0200

    added the study drills as comments, will take more time than i have now to complete.

--- ex13.py
+++ ex13.py
@@ -8,3 +8,10 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+
+# Write a script that has fewer arguments and one that has more, use interesting names for the unpacked Variables
+
+# Combine the use of input() with that of argv to make a script that gets more input from the user (in different ways)
+
+# HAVE FUN DOING THIS, TAKE YOUR TIME...no rush.(name the new scripts like - ex13-1.py, ex13-2.py, ... you can even consider putting htem in a folder, but that just makes running htem harder, although it could help in practising how to nav in and out of a dir.)
