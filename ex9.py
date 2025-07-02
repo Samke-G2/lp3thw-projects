db52a520ac180cf48f38183a8c5ad15858b80210
Samkelwa <samkelwa101@gmail.com>
2025-07-02 21:24:11 +0200

    Started and completed exercise 9, working on hte study drills.

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# Exercise 9 - More printing, printing, printing
+
+# Here we create a variable storing the days of the week, in one line seperated by a space
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Here we create a variable storing the months, each on a different line with the '\n' character
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+#
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
