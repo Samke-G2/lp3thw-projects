5eb877f1236f6d817781598efe43b9c9410e1377
Samkelwa <samkelwa101@gmail.com>
2025-07-03 20:49:59 +0200

    finished all three study drills for exercise 13, and removed the instructions from the ex13.py file. on to the next.

---
+++ exerc13/ex13-3.py
@@ -0,0 +1,12 @@
+# Exercise 13-3.py - arguments and input in one
+
+from sys import argv
+
+script, un, deu, trois1, trois2 = argv
+
+print(f" Can you feel it? \n {un}! \n Chommie! \n {deu}! \n Chommie! \n {trois1} {trois2}!")
+
+input("Sihamba sonke! ")
+input("kusho bani? ")
+input("Kusho bani, mahn? ")
+input("Sihamba sonke... ")
