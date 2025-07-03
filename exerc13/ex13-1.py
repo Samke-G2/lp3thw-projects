c18d24aba63361c14d3a8001f9dd070d68c4f70b
Samkelwa <samkelwa101@gmail.com>
2025-07-03 20:01:28 +0200

    opened a exercise 13 folder, to practice unpacking argv. 2 scripts done so far, one left.

---
+++ exerc13/ex13-1.py
@@ -0,0 +1,7 @@
+# Exercise 13.1 - A script with more inputs
+
+from sys import argv
+
+script, first, second, third, fourth = argv
+
+print(f"Then Darth Vader looked at him and said, \"Luke, {first} {second} {third} {fourth}! \" ")
---
+++ exerc13/ex13-2.py
@@ -0,0 +1,7 @@
+# Exercise 13-2 - A script with fewer arguments
+
+from sys import argv
+
+script, uno, dos = argv
+
+print(f"Halala! {uno}! Yey wena! {dos}!")
