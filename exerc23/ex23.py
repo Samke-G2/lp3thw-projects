6e12e7c085f9f137c9f4b66d66d1aae3c585c401
Samkelwa <samkelwa101@gmail.com>
2025-07-14 10:45:24 +0200

    Finished exercise 23, typing out the file. idk about study drills, just commiting to save and close.

--- exerc23/ex23.py
+++ exerc23/ex23.py
@@ -12,6 +12,8 @@ This program is created to learn 4 things:
 3. How to handle errors in string and byte handling.
 
 4. How to read code and find out what it means even if previously unseen.
+
+*UTF-8 = Unicode Transformation Format 8 Bits
 """
 import sys
 script, input_encoding, error = sys.argv
@@ -25,4 +27,13 @@ def main(language_file, encoding, errors):
         return main(language_file, encoding, errors)
 
 def print_line(line, encoding, errors):
-    
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, input_encoding, error )
