3ad1f30eb9161703f7c820e4ce5f64f0adaebfed
Samkelwa <samkelwa101@gmail.com>
2025-07-16 23:50:55 +0200

    typed some things into the ex24 file. until the calculation after the poem.

--- ex24.py
+++ ex24.py
@@ -9,3 +9,18 @@ This is a practice exercise to recap all the other exercises done until this poi
 print("Let's practice everything.")
 print('You\'d need to know \'bout escape with \\ that do:')
 print("\n newlines and \t tabs.")
+
+poem = """
+\tThe lovely world
+with logic is so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("--------------")
+print(poem)
+print("--------------")
+
+five = 10 - 2 + 3 - 6
