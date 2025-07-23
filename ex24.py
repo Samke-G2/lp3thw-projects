90528383857cae06d1656c1e57fe6a93628d526d
Samkelwa <samkelwa101@gmail.com>
2025-07-23 15:26:21 +0200

    Finished ex24, the typed file yet not the study drills.

--- ex24.py
+++ ex24.py
@@ -27,3 +27,24 @@ five = 10 - 2 + 3 - 6
 print(f"This should be five: {five}")
 
 def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 1000
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+# Remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# It's just like with an f-string
+print(f"We'd have {beans} beans, {jars} jars, and {creates} crates.")
+
+start_point = start_point / 10
+
+print("We can also do that this way:")
+formula = secret_formula(start_point)
+
+# this is an easy way to apply a list to format string
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
