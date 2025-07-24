4651ec8b9744c29338090014aba5fbbf1462a603
Samkelwa <samkelwa101@gmail.com>
2025-07-25 01:12:10 +0200

    Finished ex24 with all its study drills. It wasn't 100% perfect in hte previous commit, it is now.

--- ex24.py
+++ ex24.py
@@ -5,7 +5,6 @@ author: samke_g2
 This is a practice exercise to recap all the other exercises done until this point and practice the important stuff.
 """
 
-
 print("Let's practice everything.")
 print('You\'d need to know \'bout escape with \\ that do:')
 print("\n newlines and \t tabs.")
@@ -18,6 +17,7 @@ nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
 """
+# Line 18 has a double tab and it move the line further right than the tab escape in line 13.
 
 print("--------------")
 print(poem)
@@ -29,22 +29,28 @@ print(f"This should be five: {five}")
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars / 1000
+    crates = jars / 100
     return jelly_beans, jars, crates
+# The function returns three values, assigned in that order to any we call it to (like unpacking the argv method)
+
 
 
 start_point = 10000
+# Here we assign the values returned by the function to three different variables IN ONE LINE!
 beans, jars, crates = secret_formula(start_point)
 
 # Remember that this is another way to format a string
+# the '.format()' method does what the 'f' at the beginning of a string does, just that the formatted bits need to be arguments of the method instead of directly in the string
 print("With a starting point of: {}".format(start_point))
 # It's just like with an f-string
-print(f"We'd have {beans} beans, {jars} jars, and {creates} crates.")
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
+# Here we modify the starting point slightlty from the original, like reassigning it basically
 start_point = start_point / 10
 
 print("We can also do that this way:")
 formula = secret_formula(start_point)
 
 # this is an easy way to apply a list to format string
-print("We'd have {} beans, {} jars, and {} crates.".format(*formula)) 
+# Like it says, we apply a list to a format string, but now we bypass the need for variable names to carry the values returned by the function
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
