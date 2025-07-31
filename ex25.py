d761cdf88f287214ac1c4a06bf8dd90dae5e56d2
Samkelwa <samkelwa101@gmail.com>
2025-07-31 02:06:16 +0200

    Fixed some bugs from the other version, looks fine now.

--- ex25.py
+++ ex25.py
@@ -5,14 +5,11 @@ author: Samke_g2 via Zed Shaw
 Exercise 25 of the "Learn python the hard way" book, its basically practice on
 strings and lists.
 
-Now, we need to learn how to import into powershell 7 and work it out, or maybe revert to the other powersehell.
-Maybe find out how to create own module and access functions from it. Thats what ex25 is really about.
-
 """
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(" ")
-    return break_words
+    return words
 
 def sort_words(words):
     """Sorts the words."""
@@ -31,7 +28,7 @@ def print_last_word(words):
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    return sorte_words(words)
+    return sorted(words)
 
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence. """
