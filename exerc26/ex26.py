71a4844f8e1751d6fcf573762c382fdb35a9b96c
Samkelwa <samkelwa101@gmail.com>
2025-08-01 00:22:12 +0200

    I think I finished debugging the file, yet to test it tho.

--- exerc26/ex26.py
+++ exerc26/ex26.py
@@ -4,33 +4,34 @@ author: Samke_G2 via Zed Shaw
 
 Essentially I'm debugging a buggy file created by Zed Shaw so that it works as expected.
 """
+from sys import argv
+
 print("How old are you?", end=' ')
 age = input()
-print("How tall are you?", end=' ')
+print("How tall are you (in m)?", end=' ')
 height = input()
-print("How much do you weigh?", end=' '
+print("How much do you weigh (in kg)?", end=' '
 weight = input()
 
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print(f"So, you're {age} years old, {height}m tall and weigh {weight}kg.")
 
 script, filename = argv
 
-txt = open(filenme)
+txt = open(filename)
 
-print("Here's your file {filename}:")
-print(tx.read())
+print(f"Here's your file {filename}:")
+print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
+print(txt_again.read())
 
 
-print('Let's practice everything.')
-print('You\'d need to know \'bout escapes
-      with \\ that do \n newlines and \t tabs.')
+print('Let\'s practice everything.')
+print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
 
 poem = """
 \tThe lovely world
@@ -41,23 +42,23 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-print("--------------)
+print("--------------")
 print(poem)
-print(--------------")
+print("--------------")
 
 
-five = 10 - 2 + 3 -
-print(f"This should be five: {five}"
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
 
-def secret_formula(started)
+def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars  100
+    crates = jars * 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
@@ -74,20 +75,20 @@ print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 people = 20
-cates = 30
+cats = 30
 dogs = 15
 
 
 if people < cats:
-    print "Too many cats! The world is doomed!"
+    print("Too many cats! The world is doomed!")
 
-if people < cats:
+if people > cats:
     print("Not many cats! The world is saved!")
 
 if people < dogs:
     print("The world is drooled on!")
 
-if people > dogs
+if people > dogs:
     print("The world is dry!")
 
 
@@ -96,9 +97,9 @@ dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
-    print("People are less than or equal to dogs.)
+if people <= dogs:
+    print("People are less than or equal to dogs.")
 
 
-if people = dogs:
+if people == dogs:
     print("People are dogs.")
