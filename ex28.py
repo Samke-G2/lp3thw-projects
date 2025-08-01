4f7c44c9d9016b8aeb0c8d878f2ee618bd753511
Samkelwa <samkelwa101@gmail.com>
2025-08-01 03:29:36 +0200

    Started the boolean tables bit. I think i know booleans so i'm not taking a week to memorise the tables. This exercise will show if i really can.

---
+++ ex28.py
@@ -0,0 +1,13 @@
+"""
+Exercise 28 - Boolean practice
+author: Samke_G2 via Zed Shaw
+
+This file is meant to be typed into the terminal line-by-line.
+I type it here as a way to practice my typing skill and to record a github commit.
+I will register my guesses as comments on the same line as the booleans.
+"""
+True and True
+
+False and True
+
+1 == 1 and 2 == 1
