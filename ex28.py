ff65f013b332a7e05ad08860e4b24458a26019e4
Samkelwa <samkelwa101@gmail.com>
2025-08-05 16:21:35 +0200

    Changed the names of the files, added zeros for the single-digit ones. modified the ex28 file to include my attempts at answering.

--- ex28.py
+++ ex28.py
@@ -6,38 +6,38 @@ This file is meant to be typed into the terminal line-by-line.
 I type it here as a way to practice my typing skill and to record a github commit.
 I will register my guesses as comments on the same line as the booleans.
 """
-True and True      
+True and True           # True
 
-False and True
+False and True          # False
 
-1 == 1 and 2 == 1
+1 == 1 and 2 == 1       # False
 
-"test" == "test"
+"test" == "test"        # True
 
-1 == 1 or 2 != 1
+1 == 1 or 2 != 1        # True
 
-True and 1 == 1
+True and 1 == 1         # True
 
-False and 0 != 0
+False and 0 != 0        # False
 
-True or 1 == 1
+True or 1 == 1          # True
 
-"test" != "testing"
+"test" != "testing"     # True
 
-"test" == 1
+"test" == 1             # False
 
-not (True and False)
+not (True and False)    # True
 
-not (1 == 1 and 0 != 1)
+not (1 == 1 and 0 != 1)                 # False
 
-not (10 ==1 or 1000 == 1000)
+not (10 == 1 or 1000 == 1000)           # False
 
-not (1 != 10 or 3 == 4)
+not (1 != 10 or 3 == 4)                 # False
 
-not ("testing" == "testing" and "Sam" == "Cool Guy")
+not ("testing" == "testing" and "Sam" == "Cool Guy")                    # True
 
-1 == 1 and (not("testing" == 1 or 1 == 0))
+1 == 1 and (not("testing" == 1 or 1 == 0))                              # True
 
-"chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3))                        # False
 
-3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))          # False
