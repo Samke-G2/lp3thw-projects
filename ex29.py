7a548601b0c2a87168f46786d0be20d94db0556b
Samkelwa <samkelwa101@gmail.com>
2025-08-05 16:57:24 +0200

    created the ex29 file, but it currently only has the docstring in it.

---
+++ ex29.py
@@ -0,0 +1,6 @@
+"""
+Exercise 29 - What if
+author: Samke_G2 (all exercises in this repo should be assumed to be typed by me under the direction of the "Learn Python 3 The Hard Way" book by Zed Shaw)
+
+This particular exercise seems to be a practice exercise on 'if' statements combined with the use of booleans.
+"""
