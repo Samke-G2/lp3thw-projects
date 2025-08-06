4b55116c735a8084fe6a70fa5e3154b4a2a500ea
Samkelwa <samkelwa101@gmail.com>
2025-08-06 16:56:26 +0200

    Finished typing ex29 file, no test or debug.

--- ex29.py
+++ ex29.py
@@ -4,3 +4,31 @@ author: Samke_G2 (all exercises in this repo should be assumed to be typed by me
 
 This particular exercise seems to be a practice exercise on 'if' statements combined with the use of booleans.
 """
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
