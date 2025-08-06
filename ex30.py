d5f57ebd69d095113a1d8decc4286cf50ed65afe
Samkelwa <samkelwa101@gmail.com>
2025-08-06 17:04:49 +0200

    Finished ex29 and startted a file for ex30.

---
+++ ex30.py
@@ -0,0 +1,6 @@
+"""
+Exercise 30 - Else and If
+author: Samke_G2
+
+In this exercise I practice the else-if statements. It's just to reinforce the concept of branches and conditionals, it think.
+"""
