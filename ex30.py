60f739fa441ef4a62e64b0caf83b8c961183d83d
Samkelwa <samkelwa101@gmail.com>
2025-08-06 17:10:45 +0200

    Finished typing the ex30 file.

--- ex30.py
+++ ex30.py
@@ -4,3 +4,26 @@ author: Samke_G2
 
 In this exercise I practice the else-if statements. It's just to reinforce the concept of branches and conditionals, it think.
 """
+people = 30
+cars = 40
+trucks = 15
+
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should not take the cars.")
+else:
+    print("We can't decide")
+
+if trucks > cars:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("We still can't decide.")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
