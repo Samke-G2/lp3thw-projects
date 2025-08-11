d21e03d89ddd8b97d46870cb7a4ff43ba66e8ed8
Samkelwa <samkelwa101@gmail.com>
2025-08-11 22:05:56 +0200

    Fixed a minor error in the ex31.py file, created the study drill file to create my own flow.

---
+++ exerc31/greeting.py
@@ -0,0 +1,12 @@
+"""
+file: greeting.py
+author: Samke_G2
+
+This file is created as part of the exercise 31 study drills where I have to create my own "choose your own adventure" type
+file that uses if statements and code branches to got to a solution in several small steps.
+"""
+print("""
+This program helps you figure out if you should say hi to someone you know, and have just seen, in several steps.
+We start at an inital question and go from there,
+let's get started..
+""")
