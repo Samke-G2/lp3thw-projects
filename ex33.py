b52bd5784e6d006f8367a888407b2de22b901b9d
Samkelwa <samkelwa101@gmail.com>
2025-08-13 14:24:02 +0200

    started the ex33.py file on while loops, just docstring for now.

---
+++ ex33.py
@@ -0,0 +1,11 @@
+"""
+Exercise 33 - While Loops
+author: Samke_g2
+
+Learning while-loops while doing th three checks to make sure they don't run forever:
+    1. Make sure that you use while-loops sparingly. Usually a for-loop is better
+    2. Review your while statements and make sure that the boolean test will become False at some
+        point.
+    3. When in doubt,print out your test variable at the top and bottom of the while-loop toseewhat
+        it’s doing.
+"""
