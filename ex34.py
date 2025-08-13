ac6e4552e58a47362a43807c11e2765ff51ac87b
Samkelwa <samkelwa101@gmail.com>
2025-08-13 16:35:31 +0200

    Started, typed, and finished the short ex34.py file and all its study drills.

---
+++ ex34.py
@@ -0,0 +1,23 @@
+"""
+Exercise 34 - Accessing Elements of Lists
+author: Samke_g2
+
+As simple program that pulls list elements out of a list by accessing their indeces
+"""
+animals = ["bear", "python3.6", "peacock", "kangaroo", "whale", "platypus"]
+
+print(f"1. The animal at 1 is: {animals[1]}\n")
+
+print("2. The third animal is: ", animals[2], "\n")
+
+print(f"3. The first animal is: {animals[0]}\n")
+
+print("4. The animal at 3 is: ", animals[3], "\n")
+
+print(f"5. The fifth animal is: {animals[4]}\n")
+
+print("6. The animal at 2 is: ", animals[2], "\n")
+
+print(f"7. The sixth animal is: {animals[5]} \n")
+
+print(f"8. The animal at 4 is: {animals[4]}\n")
