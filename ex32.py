170051c7e878d320f11cb93b8d757c989264df37
Samkelwa <samkelwa101@gmail.com>
2025-08-13 13:57:18 +0200

    started and finished the ex32.py file. starting on study drills.

---
+++ ex32.py
@@ -0,0 +1,34 @@
+"""
+Exercise 32 - Loops and Lists
+author: Samke_g2
+
+Building some lists using some for - loops and printing them out
+"""
+the_count = [1, 2, 3, 4, 5]
+fruits = ["apples", "oranges", "pears", "apricots"]
+change = [1, "pennies", 2, "dimes", 3, "quarters"]
+
+# this first kind of for-loops goes through a lists
+for number in the_count:
+    print(f"This is count {number}")
+
+# same as above
+for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+# also we can go through mixed lists too
+for i in change:
+    print(f"I got {i}")
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range function to do o to 5 counts
+for i in range(0, 6):
+    print(f"Adding {i} to the list.")
+    # append is a function that lists understand
+    elements.append(i)
+
+# now we can print them out too
+for i in elements:
+    print(f"Element was: {i}")
