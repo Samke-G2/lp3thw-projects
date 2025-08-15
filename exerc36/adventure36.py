75f9819631e0c0a8678720a0b902b008846cd84d
Samkelwa <samkelwa101@gmail.com>
2025-08-15 16:42:47 +0200

    Created a file for the game i have to create in exercise 36, the file has just the docstring atm.

---
+++ exerc36/adventure36.py
@@ -0,0 +1,8 @@
+"""
+filename = adventure36.py
+author: Samke_g2
+
+This program is a text-based adventure game that gives the user choices and takes the user through different scenarios based on their inputs.
+Created as part of the Exercise 36 study drill in the "Learn Python 3 the Hard Way" book by Zed Shaw.
+
+"""
