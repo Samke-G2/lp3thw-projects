afe978278abf8c5bcca5311f6c5fb2ce8d39c48d
Samkelwa <samkelwa101@gmail.com>
2025-08-21 22:26:18 +0200

    Started exercise 42, just the docstring.

---
+++ ex42.py
@@ -0,0 +1,6 @@
+"""
+Exercise 42 - Is-A, Has-A, Objects and Classes
+author: Samke_g2
+
+This programme consolidates hte knowledge of classes and objects while highlighting the differences between clases and objects.
+"""
