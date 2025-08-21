535ef6b07711b1b56e770a42429d59291acfa4db
Samkelwa <samkelwa101@gmail.com>
2025-08-21 16:51:38 +0200

    Finished the ex40.py while and tested it, it works. Now to do the study drills...

--- ex40.py
+++ ex40.py
@@ -5,3 +5,22 @@ author: Samke_g2
 In this program we go through the principles of OOP (Object-oriented programming)
 and what the benefits of using classes & objects are.
 """
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
