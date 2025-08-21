e1a5ac9c5e27b5d1be7be34cda2f704df8cd5bb4
Samkelwa <samkelwa101@gmail.com>
2025-08-22 00:02:35 +0200

    added one class to the code, not far.

--- ex42.py
+++ ex42.py
@@ -4,3 +4,6 @@ author: Samke_g2
 
 This programme consolidates hte knowledge of classes and objects while highlighting the differences between clases and objects.
 """
+#  Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
