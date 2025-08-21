2fb021c38b75ba8319d70654f0580f1d39f20101
Samkelwa <samkelwa101@gmail.com>
2025-08-21 16:36:17 +0200

    Finished ex39 and all its study drills. started on ex40, just the docstring

---
+++ ex40.py
@@ -0,0 +1,7 @@
+"""
+Exercise 40 - Modules, Classes, and Objects
+author: Samke_g2
+
+In this program we go through the principles of OOP (Object-oriented programming)
+and what the benefits of using classes & objects are.
+"""
